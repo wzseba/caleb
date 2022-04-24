53acd296400d2e3bdfec4647f3e56acdb8ee382b
wzseba <wzseba@gmail.com>
2022-04-24 10:53:28 -0300

    permutations

---
+++ 14-multiplication-table/app.js
@@ -0,0 +1,23 @@
+/*
+Your task, is to create NxN multiplication table, of size provided in parameter.
+
+for example, when given size is 3:
+
+1 2 3
+2 4 6
+3 6 9
+for given example, the return value should be: [[1,2,3],[2,4,6],[3,6,9]]
+*/
+multiplicationTable = function(size) {
+    // insert code here
+    let arr = [];
+    for(let i = 1 ; i <= size; i++){
+    let fila = [];
+      for(let j = 1; j <= size; j++){
+          fila.push(i*j);
+      }
+         arr.push(fila);
+    }
+    return arr;
+}
+multiplicationTable(3);
